58005ef82b9e29f6780645b021643d75de10bac7
Akshat-Tated <akshattated18@gmail.com>
2026-03-28 16:24:31 +0530

    Added comments

--- task_1.py
+++ task_1.py
@@ -1,4 +1,23 @@
+"""## Practical Exercise 1
+
+### Title
+**Automate Product Selection and Delivery Check on ShoppersStack**
+
+### Description
+Open the ShoppersStack website using Selenium WebDriver (https://www.shoppersstack.com/).
+
+Automate the process of selecting a product category and verifying delivery availability using a pincode.
+
+Perform the following steps:
+- Launch the browser and open the ShoppersStack website
+- Maximize the browser window
+- Click on the **"APPLE"** product category
+- Locate the delivery input field and enter the pincode
+- Click on the **"Check"** button"""
+
 from selenium.webdriver import Chrome,ChromeOptions
+#ChromeOptions to customize browser settings
+
 from selenium.webdriver.common.by import By
 from time import sleep
 
@@ -7,19 +26,34 @@ from selenium.webdriver.support import expected_conditions as EC
 
 o = ChromeOptions()
 o.add_experimental_option("detach",True)
+# Keep browser open even after script execution
+
 driver = Chrome(options=o)
+# Launch Chrome browser with given options
+
 
 driver.get("https://www.shoppersstack.com/")
+# Open the target website
+
 driver.maximize_window()
+# Maximize browser window
+
 driver.implicitly_wait(100)
+# Apply implicit wait (global wait for elements to load)
 
 driver.find_element(By.XPATH,'//img[@src="https://m.media-amazon.com/images/I/71ZDY57yTQL._AC_UY327_FMwebp_QL65_.jpg"]').click()
+# Located and clicked on product image using XPath
 
 driver.find_element(By.XPATH,'//input[@id="Check Delivery"]').send_keys('123456')
+# Located pincode input field and entered value
+
 sleep(4)
 wait = WebDriverWait(driver, 10)
 ele = wait.until(EC.visibility_of_element_located((By.XPATH,'//button[@id="Check"]')))
+# Explicit wait to pause execution until the element is visible and accessible
 driver.find_element(By.XPATH,'//button[@id="Check"]').click()
 sleep(4)
+# Wait to observe result
 
 driver.quit()
+
--- task_2.py
+++ task_2.py
@@ -1,4 +1,27 @@
+"""## Practical Exercise 2
+
+### Title
+**Automate Myntra Product Selection**
+
+### Description
+Open the Myntra website using Selenium WebDriver (https://www.myntra.com/).
+
+Automate the process of navigating categories, selecting a product, applying filters, sorting products and adding to bag.
+
+Perform the following steps:
+- Launch the browser and open the Myntra website
+- Maximize the browser window
+- Hover over the **Genz** category
+- Click on **"Jackets Under ₹899"**
+- Select any 2 filter under the product filters (e.g., brand, size, or color)
+- Click on the **Sort By** 'Popularity'
+- Click on the any one product
+- Select size (if mentioned)
+- Add to bag"""
+
 from selenium.webdriver import Chrome,ChromeOptions
+#ChromeOptions to customize browser settings
+
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -8,6 +31,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 o = ChromeOptions()
 o.add_experimental_option("detach",True)
+# Keep browser open even after script execution
+
 o.add_argument("--disable-notifications")
 driver = Chrome(options=o)
 #driver is holding the browser
@@ -19,31 +44,44 @@ driver.implicitly_wait(100)
 actions = ActionChains(driver)
 ele = driver.find_element(By.XPATH,'//a[@href="/shop/fwd-women"][1]')
 actions.move_to_element(ele).pause(2).perform()
+#using actionchains to hover to the element
 
 wait = WebDriverWait(driver, 10)
 ele = wait.until(EC.visibility_of_element_located((By.XPATH,'//a[@data-reactid="837"]')))
+# Explicit wait to pause execution until the element is visible and accessible
 ele.click()
 
 gender = driver.find_element(By.XPATH,'//input[@value="men,men women"]')
 actions.click(gender).perform()
+#click the radio button
 sleep(3)
 
 brand = driver.find_element(By.XPATH,'//input[@value="Rigo"]')
 actions.click(brand).perform()
+#slecting brand="rigo" checkbox
 sleep(3)
 
 sort_by = driver.find_element(By.XPATH,'//div[@class="sort-sortBy"]')
 actions.move_to_element(sort_by).pause(2).release().perform()
+#hover to the dropdown to select options
 
 hover_ele = driver.find_element(By.XPATH,'//ul[@class="sort-list"]//li[3]//input')
 actions.move_to_element(sort_by).click().perform()
 # driver.find_element(By.XPATH,'//input[@value="popularity"]').click()
 sleep(3)
+# Wait to observe result
+
 driver.find_element(By.XPATH,'//img[@title="Rigo Typography Printed Fleece Oversized Varsity Jacket"]').click()
 sleep(3)
+# Wait to observe result
+
 driver.switch_to.window(driver.window_handles[1])
 driver.find_element(By.XPATH,'//div[@class="size-buttons-buttonContainer"][1]').click()
 sleep(3)
+# Wait to observe result
+
 driver.find_element(By.XPATH,'//span[@class="myntraweb-sprite pdp-whiteBag sprites-whiteBag pdp-flex pdp-center"]').click()
 sleep(3)
+# Wait to observe result
+
 driver.quit()
